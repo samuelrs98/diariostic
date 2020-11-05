effc9738dd2bf3ad204e9798d057699b9046f588
Samuel Rodríguez Simón <73548835+samuelrs98@users.noreply.github.com>
2020-11-05 11:28:31 +0100

    Update portada.php

--- secciones/portada.php
+++ secciones/portada.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada actualizada",
-"autor" => "mtorres",
+"titulo" => "Portada actualizada otra vez",
+"autor" => "srs241",
 "resumen" => "Resumen de la portada",
 ];
 ?>
